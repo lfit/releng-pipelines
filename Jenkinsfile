349b4f36f9b1a9756be2022b3f1e87c5720052f2
Eric Ball <eball@linuxfoundation.org>
2020-06-17 19:20:15 -0700

    Initial commit, log shipping functionality
    This is an initial commit of log shipping, linting, and unit testing.
    TODO:
      * Add tox-like precommit testing in gradle
      * Create docs/changelog
    
    Issue: RELENG-2792
    Change-Id: Id5f822fde87352304f302ae964b5af0fe075bdf9
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

---
+++ Jenkinsfile
@@ -0,0 +1,83 @@
+//
+// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2020 The Linux Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+loadGlobalLibrary()
+
+pipeline {
+    agent {
+        label 'centos7-docker-4c-2g'
+    }
+
+    options {
+        timestamps()
+        timeout(360)
+    }
+
+    stages {
+        stage('Lint Pipelines') {
+            steps {
+                sh './scripts/pipeline-linter.sh'
+            }
+        }
+
+        stage('Test') {
+            agent {
+                docker {
+                    image "gradle"
+                    reuseNode true
+                }
+            }
+            steps {
+                sh 'gradle -Dgradle.user.home=/gradleCache clean test'
+
+                junit allowEmptyResults: true, testResults: 'target/test-results/test/*.xml'
+
+                // Test summary
+                publishHTML([
+                    allowMissing: true,
+                    alwaysLinkToLastBuild: true,
+                    keepAll: true,
+                    reportDir: 'target/reports/tests/test',
+                    reportFiles: 'index.html',
+                    reportName: 'Unit Test Summary'
+                ])
+            }
+        }
+
+    post {
+        failure {
+            script {
+                currentBuild.result = "FAILED"
+            }
+        }
+    }
+}
+
+def loadGlobalLibrary(branch = '*/master') {
+    library(identifier: 'pipelines@master',
+        retriever: legacySCM([
+            $class: 'GitSCM',
+            userRemoteConfigs: [[url: 'https://gerrit.linuxfoundation.org/infra/releng/pipelines']],
+            branches: [[name: branch]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[
+                $class: 'SubmoduleOption',
+                recursiveSubmodules: true,
+            ]]]
+        )
+    ) _
+}
---
+++ vars/lfInfraShipLogs.groovy
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2020 The Linux Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Method to ship logs. It is meant to be called directly, not
+ * instantiated, e.g.
+ * lfInfraShipLogs{}
+ * or
+ * lfInfraShipLogs{ logSettingsFile = myLogSettingsFileName }
+ *
+ * @param body Config values to be provided in the form "key = value".
+ */
+def call(body) {
+    // Evaluate the body block and collect configuration into the object
+    def config = [:]
+
+    if(body) {
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+    }
+
+    def _logSettingsFile = config.logSettingsFile ?: 'jenkins-log-archives-settings'
+    if (!_logSettingsFile) {
+        throw new Exception('Log settings file id (logSettingsFile) is required for LF log deploy script.')
+    }
+
+    withEnv(["SERVER_ID=logs"]){
+        configFileProvider([configFile(fileId: _logSettingsFile, variable: 'SETTINGS_FILE')]) {
+            echo 'Running shell/create-netrc.sh'
+            sh(script: libraryResource('shell/create-netrc.sh'))
+        }
+
+        echo 'Running shell/python-tools-install.sh'
+        sh(script: libraryResource('shell/python-tools-install.sh'))
+        echo 'Running shell/sudo-logs.sh'
+        sh(script: libraryResource('shell/sudo-logs.sh'))
+        echo 'Running shell/job-cost.sh'
+        sh(script: libraryResource('shell/job-cost.sh'))
+
+        buildDesc = ""
+
+        if ("$S3_BUCKET" =~ /.*logs-s3.*/) {
+            configFileProvider([configFile(fileId: "jenkins-s3-log-ship",
+                                           targetLocation: '$HOME/.aws/credentials')]) {
+                echo 'Running shell/logs-deploy.sh'
+                sh(script: libraryResource('shell/logs-deploy.sh'))
+            }
+            buildDesc = "S3 build logs: <a href=\"https://$CDN_URL/$s3_path\"></a"
+        } else if ("$LOGS_SERVER" != "") {
+            echo 'Running shell/logs-deploy.sh'
+            sh(script: libraryResource('shell/logs-deploy.sh'))
+            buildDesc = "Nexus build logs: <a href=\"$LOGS_SERVER/$nexus_path\">$LOGS_SERVER/$nexus_path</a>"
+        }
+
+        echo 'Running shell/logs-clear-credentials.sh'
+        sh(script: libraryResource('shell/logs-clear-credentials.sh'))
+    }
+
+    // Set build description with build logs and PR info if applicable
+    if (!currentBuild.description) {currentBuild.description = ''}
+
+    // the old GHPRB plugin updated the description to contain the PR # with a link to the PR.
+    // If the build description contains a link to the PR then add a br
+    if (currentBuild.description.contains('PR #')) {
+        currentBuild.description += "<br>"
+    }
+    currentBuild.description += buildDesc
+}
