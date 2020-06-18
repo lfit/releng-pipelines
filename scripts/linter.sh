659bfc9f96759ba9877da26f81fc06ad09999c54
Eric Ball <eball@linuxfoundation.org>
2020-06-17 19:20:15 -0700

    Initial commit, log shipping functionality
    This is an initial commit of log shipping, linting, and unit testing.
    There is still work to be done here (there are still a couple TODOs
    in the unit test), but this can at least be given review and feedback
    while the final version is being tested/completed.
    
    Issue: RELENG-2792
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>
    Change-Id: Id5f822fde87352304f302ae964b5af0fe075bdf9

---
+++ scripts/linter.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+JENKINS_URL=https://jenkins.edgexfoundry.org
+JENKINS_CRUMB=`curl --silent "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"`
+JENKINS_VAL="$JENKINS_URL/pipeline-model-converter/validate"
+JENKINS_FILE_LIST=(`grep -lr "pipeline\s*{" vars src`)
+
+for JENKINS_FILE in "${JENKINS_FILE_LIST[@]}"
+do
+    ret=$(curl --silent -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<$JENKINS_FILE" $JENKINS_VAL)
+    if [[ $ret == *"Errors"* ]];then
+        echo $ret
+        echo "Linting has failed for $JENKINS_FILE"
+        nonZeroexit=true
+    else
+        echo "$JENKINS_FILE successfully validated"
+        echo $ret
+    fi
+done
+
+# set non-zero exit if linter reports any errors
+if [ $nonZeroexit ]
+then
+    exit 1
+fi
---
+++ src/test/groovy/lfInfraShipLogsSpec.groovy
@@ -0,0 +1,73 @@
+import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+import spock.lang.Ignore
+
+public class LFInfraShipLogsSpec extends JenkinsPipelineSpecification {
+
+    def lfInfraShipLogs = null
+
+    def setup() {
+        lfInfraShipLogs = loadPipelineScriptForTest('vars/lfInfraShipLogs.groovy')
+        explicitlyMockPipelineVariable('out')
+    }
+
+    def "Test lfInfraShipLogs [Should] throw exception [When] logSettingsFile is null" () {
+        setup:
+        when:
+            lfInfraShipLogs({logSettingsFile = null})
+        then:
+            thrown Exception
+    }
+
+    def "Test lfInfraShipLogs [Should] call expected shell scripts with expected arguments [When] called" () {
+        setup:
+            def environmentVariables = [
+                'DOCKER_REGISTRY': 'MyDockerRegistry',
+                'ghprbPullId': true
+            ]
+            lfInfraShipLogs.getBinding().setVariable('env', environmentVariables)
+            explicitlyMockPipelineStep('echo')
+            explicitlyMockPipelineStep('withEnv')
+            getPipelineMock("libraryResource")('shell/create-netrc.sh') >> {
+                return 'create-netrc'
+            }
+            getPipelineMock("libraryResource")('shell/python-tools-install.sh') >> {
+                return 'python-tools-install'
+            }
+            getPipelineMock("libraryResource")('shell/sudo-logs.sh') >> {
+                return 'sudo-logs'
+            }
+            getPipelineMock("libraryResource")('shell/job-cost.sh') >> {
+                return 'job-cost'
+            }
+            getPipelineMock("libraryResource")('shell/logs-deploy.sh') >> {
+                return 'logs-deploy'
+            }
+            getPipelineMock("libraryResource")('shell/logs-clear-credentials.sh') >> {
+                return 'logs-clear-credentials'
+            }
+            lfInfraShipLogs.getBinding().setVariable('currentBuild', [:])
+            lfInfraShipLogs.getBinding().setVariable('LOGS_SERVER', 'MyLogServer')
+            lfInfraShipLogs.getBinding().setVariable('S3_BUCKET', 'my-logs-s3')
+            lfInfraShipLogs.getBinding().setVariable('SILO', 'MySilo')
+            lfInfraShipLogs.getBinding().setVariable('JENKINS_HOSTNAME', 'MyJenkinsHostname')
+            lfInfraShipLogs.getBinding().setVariable('JOB_NAME', 'MyJobName')
+            lfInfraShipLogs.getBinding().setVariable('BUILD_NUMBER', 'MyBuildNumber')
+        when:
+            lfInfraShipLogs()
+        then:
+            1 * getPipelineMock('withEnv').call(_) >> { _arguments ->
+                def envArgs = [
+                    'SERVER_ID=logs'
+                ]
+                assert envArgs == _arguments[0][0]
+            }
+            1 * getPipelineMock('sh').call([script:'create-netrc'])
+            1 * getPipelineMock('sh').call([script:'python-tools-install'])
+            1 * getPipelineMock('sh').call([script:'sudo-logs'])
+            1 * getPipelineMock('sh').call([script:'job-cost'])
+            // TODO: logs-deploy should test both possible conditions
+            1 * getPipelineMock('sh').call([script:'logs-deploy'])
+            1 * getPipelineMock('sh').call([script:'logs-clear-credentials'])
+            // TODO: Check value of currentBuild.description mock variable if possible
+    }
+}
