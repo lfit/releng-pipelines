ca6e4441b76a648530fc760da25acf1e9205559f
Eric Ball <eball@linuxfoundation.org>
2021-01-22 15:18:37 -0800

    Add lfParallelCostCapture function
    This is a function to be run as a "post" call at the end of each
    stage within a parallel block, which will gather cost data from
    dynamically-allocated build nodes. It outputs a file titled
    "stack-cost" and stashes it. lfInfraShipLogs has been modified to
    unstash this file, which will automatically be picked up by the
    job-cost.sh script.
    
    Issue: RELENG-3207
    Change-Id: I284cd8a2f878aed4e09dacf57fe47587abeffd1b
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

---
+++ vars/lfParallelCostCapture.groovy
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2020 The Linux Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+def call() {
+    sh(script: libraryResource('shell/job-cost.sh'))
+    cost_str = sh(script: "cat $WORKSPACE/archives/cost.csv | cut -d, -f6", returnStdout: true)
+
+    try {
+        unstash "stack-cost"
+        stack_cost = sh(script: "cat stack-cost | awk '{print \$2}", returnStdout: true)
+    } catch(Exception e) {
+        stack_cost = 0
+    }
+
+    cost = (cost_str as float) + (stack_cost as float)
+    sh("echo total: ${cost} > stack-cost")
+    stash includes: "**/stack-cost", name: "stack-cost"
+}
